893c863daae68256c38ae4c42c31fad59bfc1a8d
Pello Altadill <pello_altadill@cuatrovientos.org>
2012-05-21 14:00:21 +0200

    first commit

---
+++ gen/org/cuatrovientos/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.cuatrovientos.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/cuatrovientos/android/SerONoSerAndroidActivity.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SerONoSerAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
